467210b885e4ef28d407b3c7dad4d0931af77925
anwesh0304 <anwesh0304@gmail.com>
2020-06-04 21:43:31 +0530

    Working on the feed forward code

--- DigitRecognizer/DigitRecognizer/CPP Source/trainRead.cpp
+++ DigitRecognizer/DigitRecognizer/CPP Source/trainRead.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 using namespace std ;
 
 
 #define NO_TRAINING noTrain
+#define HIDDEN_NEURONS 25
+
+int noTrain, noRow, noCol, magicNo, featDim ;
 
 int ReverseInt (int i)
 {
@@ -16,22 +21,50 @@ int ReverseInt (int i)
     return((int)ch1<<24)+((int)ch2<<16)+((int)ch3<<8)+ch4;
 }
 
-void freeTrainData (int** data, int ex)
+double getRand ()
+{
+	return rand() / (double) RAND_MAX ;
+}
+
+void freeMatrix (void** mat, int rows)
 {
 	int i ;
 
-	for (i = 0 ; i < ex ; i++)
-		free (data[i]) ;
+	for (i = 0 ; i < rows ; i++)
+		free (mat[i]) ;
 	
-	free(data) ;
+	free(mat) ;
 }
+
+// Done only upto the hidden layer for now
+void forward (int** trainData, double** theta1)
+{
+	int i, j, k ;
+	double ele ;
+
+	for (i = 0 ; i < HIDDEN_NEURONS ; i++)
+	{
+		for (j = 0 ; j < noTrain ; j++)
+		{
+			ele = 0 ;
+			for (k = 0 ; k <= featDim ; k++)
+				ele += theta1[i][k] * trainData[j][k] ;
+
+			printf ("%d ", ele) ;
+		}
+
+		printf ("\n-------------------\n") ;
+	}
+}
+
 
 
 int main ()
 {
+	srand (time(0)) ;
+
 	FILE *fp ;
 	char buf[5] ;
-	int noTrain, noRow, noCol, magicNo ;
 	unsigned char pix ;
 
 	ifstream myFile ("Train\\train_data", ios::in | ios::binary) ;
@@ -58,23 +91,39 @@ int main ()
 	noCol = ReverseInt (noCol) ;
 	cout << "No columns = " << noCol << "\n" ;
 
-	int** trainData = (int **) malloc (sizeof (int *) * NO_TRAINING) ;
+	featDim = noRow * noCol ;
 
+	int** trainData = (int **) malloc (sizeof (int *) * NO_TRAINING) ;
 	for (int i = 0 ; i < NO_TRAINING ; i++)
 	{
-		trainData[i] = (int *) malloc (sizeof (int) * 28 * 28) ;
-		for (int j = 0 ; j < 28*28 ; j++)
+		trainData[i] = (int *) malloc (sizeof (int) * (featDim + 1)) ;
+		trainData[i][0] = 1 ;
+
+		for (int j = 1 ; j <= featDim ; j++)
 		{
 			myFile.read ((char *)&pix, sizeof(unsigned char)) ;
 			trainData[i][j] = (int) pix ;
 		}
 	}
 
-
 	cout << "Done reading " << NO_TRAINING << " training examples\n" ;
 	myFile.close () ;
 
-	freeTrainData (trainData, NO_TRAINING) ;
+	double epsRand = sqrt((double)6/(featDim + HIDDEN_NEURONS + 1)) ;
+	double** theta1 = (double **) malloc (sizeof (double *) * HIDDEN_NEURONS) ;
+	for (int i = 0 ; i < HIDDEN_NEURONS ; i++)
+	{
+		theta1[i] = (double *) malloc (sizeof (double) * (featDim + 1)) ;
 
+		for (int j = 0 ; j <= featDim ; j++)
+			theta1[i][j] = 2 * epsRand * (getRand () - 0.5) ;
+	}
+
+	cout << "Created theta1 of dimensions " << HIDDEN_NEURONS << " x " << featDim + 1 << "\n" ;
+
+	forward (trainData, theta1) ;
+
+	freeMatrix ((void **)trainData, NO_TRAINING) ;
+	freeMatrix ((void **)theta1, HIDDEN_NEURONS) ;
 	return 0 ;
 }
